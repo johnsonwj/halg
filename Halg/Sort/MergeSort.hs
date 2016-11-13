e283da11eb4bf7de5e06e90c061e118ce071b65e
William Johnson <johns.willj@gmail.com>
2016-11-12 21:05:23 -0800

    Reorganize module structure

--- Halg/Sort/MergeSort.hs
+++ Halg/Sort/MergeSort.hs
@@ -1,4 +1,4 @@
-module Halg.MergeSort (mergeSort) where
+module Halg.MergeSort (sort) where
 
 -- merges two sorted lists into another sorted list
 merge :: Ord a => [a] -> [a] -> [a]
@@ -8,11 +8,11 @@ merge (x:xs) (y:ys)
   | x < y     = x : merge xs (y:ys)
   | otherwise = y : merge (x:xs) ys
 
-mergeSort :: Ord a => [a] -> [a]
-mergeSort [] = []
-mergeSort [x] = [x]
-mergeSort x = let
+sort :: Ord a => [a] -> [a]
+sort [] = []
+sort [x] = [x]
+sort x = let
   iHalf = quot (length x) 2
   firstHalf = take iHalf x
   secondHalf = drop iHalf x 
-  in merge (mergeSort firstHalf) (mergeSort secondHalf)
+  in merge (sort firstHalf) (sort secondHalf)
