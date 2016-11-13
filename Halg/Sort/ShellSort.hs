3dcf6689ab8f3f45c945e74df05d73d264626678
William Johnson <johns.willj@gmail.com>
2016-11-12 23:00:48 -0800

    Make shellsort a bit prettier (?)

--- Halg/Sort/ShellSort.hs
+++ Halg/Sort/ShellSort.hs
@@ -15,10 +15,10 @@ nSplit n xs
 -- pops a value `h' from hs, and sorts each subsequence of every h values in xs.
 hSort :: (Ord a) => [Int] -> [a] -> [a]
 hSort hs xs
-  | not (null hs) && head hs > 1  = let
-                                      h:hs' = hs
-                                      in hSort hs' $ concat . transpose . map iSort . transpose $ nSplit h xs
-  | otherwise                     = iSort xs
+  | not (null hs) && head hs > 1  =
+    let h:hs' = hs
+      in hSort hs' $ (concat . transpose . map iSort . transpose) (nSplit h xs)
+  | otherwise = iSort xs
 
 hSeq :: Int -> [Int]
 hSeq n = takeWhile (\x -> x < n `quot` 3) (iterate (\x -> 3 * x + 1) 1)
